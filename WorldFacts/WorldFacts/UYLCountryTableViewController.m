08bf85147af2fc98dbc33f38f52acc781e27b853
Keith Harrison <krh007@gmail.com>
2014-12-09 22:56:29 +0000

    Implement UISearchController

--- WorldFacts/WorldFacts/UYLCountryTableViewController.m
+++ WorldFacts/WorldFacts/UYLCountryTableViewController.m
@@ -3,7 +3,7 @@
 //  WorldFacts
 //
 //  Created by Keith Harrison http://useyourloaf.com
-//  Copyright (c) 2012 Keith Harrison. All rights reserved.
+//  Copyright (c) 2012-2014 Keith Harrison. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
@@ -33,17 +33,16 @@
 
 #import "UYLCountryTableViewController.h"
 #import "UYLCountryViewController.h"
+#import "CountryCell.h"
 #import "Country+Extensions.h"
 
-@interface UYLCountryTableViewController () <NSFetchedResultsControllerDelegate, UISearchDisplayDelegate, UISearchBarDelegate>
+@interface UYLCountryTableViewController () <NSFetchedResultsControllerDelegate, UISearchBarDelegate, UISearchResultsUpdating>
 
 @property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
 @property (strong, nonatomic) NSNumberFormatter *decimalFormatter;
 @property (strong, nonatomic) NSArray *filteredList;
 @property (strong, nonatomic) NSFetchRequest *searchFetchRequest;
-
-@property (strong, nonatomic) IBOutlet UITableViewCell *countryCell;
-@property (strong, nonatomic) UINib *countryCellNib;
+@property (strong, nonatomic) UISearchController *searchController;
 
 typedef NS_ENUM(NSInteger, UYLWorldFactsSearchScope)
 {
@@ -55,13 +54,8 @@ typedef NS_ENUM(NSInteger, UYLWorldFactsSearchScope)
 
 @implementation UYLCountryTableViewController
 
-static NSString *UYLNibCountryCellIdentifier = @"UYLNibCountryCellIdentifier";
 static NSString *UYLCountryCellIdentifier = @"UYLCountryCellIdentifier";
 static NSString *UYLSegueShowCountry = @"UYLSegueShowCountry";
-
-#define UYL_COUNTRYCELLTAG_NAME     100
-#define UYL_COUNTRYCELLTAG_CAPITAL  200
-#define UYL_COUNTRYCELLTAG_POP      300
 
 #pragma mark -
 #pragma mark === View Life Cycle Management ===
@@ -72,63 +66,70 @@ static NSString *UYLSegueShowCountry = @"UYLSegueShowCountry";
     [super viewDidLoad];
     self.title = NSLocalizedString(@"World", @"World");
 
-    // When not using storyboards the following two lines load and register the NIB
-    // for the country cell.
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didChangePreferredContentSize:)
+                                                 name:UIContentSizeCategoryDidChangeNotification object:nil];
+    self.tableView.estimatedRowHeight = 64.0;
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
     
-    // UINib *countryNib = [UINib nibWithNibName:@"CountryCell" bundle:nil];
-    // [self.tableView registerNib:countryNib forCellReuseIdentifier:UYLCountryCellIdentifier];
+    // No search results controller to display the search results in the current view
+    self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    self.searchController.searchResultsUpdater = self;
+    self.searchController.dimsBackgroundDuringPresentation = NO;
+    
+    // Configure the search bar with scope buttons and add it to the table view header
+    self.searchController.searchBar.scopeButtonTitles = @[@"Country", @"Capital"];
+    self.searchController.searchBar.delegate = self;
+    
+    self.tableView.tableHeaderView = self.searchController.searchBar;
+    
+    self.definesPresentationContext = YES;
 }
 
-// viewDidUnload is deprecated in iOS 6
-//- (void)viewDidUnload
-//{
-//    [super viewDidUnload];
-//    self.decimalFormatter = nil;
-//}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+- (void)didChangePreferredContentSize:(NSNotification *)notification
 {
-    return YES;
+    [self.tableView reloadData];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    self.decimalFormatter = nil;
+    self.searchFetchRequest = nil;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([segue.identifier isEqualToString:UYLSegueShowCountry])
     {
+        NSIndexPath *indexPath = [self.tableView indexPathForCell:sender];
         Country *country = nil;
-        if (self.searchDisplayController.isActive)
+        if (self.searchController.isActive)
         {
-            NSIndexPath *indexPath = [self.searchDisplayController.searchResultsTableView indexPathForCell:sender];
             country = [self.filteredList objectAtIndex:indexPath.row];
         }
         else
         {
-            NSIndexPath *indexPath = [self.tableView indexPathForCell:sender];
             country = [self.fetchedResultsController objectAtIndexPath:indexPath];
         }
-        UYLCountryViewController *viewController = segue.destinationViewController;
-        viewController.country = country;
+        
+        UYLCountryViewController *controller = (UYLCountryViewController *)[[segue destinationViewController] topViewController];
+        [controller setCountry:country];
+        controller.navigationItem.leftBarButtonItem = self.splitViewController.displayModeButtonItem;
+        controller.navigationItem.leftItemsSupplementBackButton = YES;
     }
-}
-
-- (void)didReceiveMemoryWarning
-{
-    self.searchFetchRequest = nil;
-    [super didReceiveMemoryWarning];
 }
 
 #pragma mark -
 #pragma mark === Accessors ===
 #pragma mark -
-
-- (UINib *)countryCellNib
-{
-    if (!_countryCellNib)
-    {
-        _countryCellNib = [UINib nibWithNibName:@"CountryCell" bundle:nil];
-    }
-    return _countryCellNib;
-}
 
 - (NSNumberFormatter *)decimalFormatter
 {
@@ -164,121 +165,81 @@ static NSString *UYLSegueShowCountry = @"UYLSegueShowCountry";
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    if (tableView == self.tableView)
+    if (self.searchController.active)
     {
-        return [[self.fetchedResultsController sections] count];
+        return 1;
     }
     else
     {
-        return 1;
+        return [[self.fetchedResultsController sections] count];
     }
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    if (tableView == self.tableView)
+    if (self.searchController.active)
     {
-        id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:section];
-        return [sectionInfo numberOfObjects];
+        return [self.filteredList count];
     }
     else
     {
-        return [self.filteredList count];
+        id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:section];
+        return [sectionInfo numberOfObjects];
     }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Note that here we are not using the tableView parameter to retrieve a new table view cell. Instead
-    // we always use self.tableView. This is necessary as our custom table view cell defined in the
-    // storyboard and is not registered with the search results table view but only with the original
-    // table.
+    CountryCell *cell = [self.tableView dequeueReusableCellWithIdentifier:UYLCountryCellIdentifier forIndexPath:indexPath];
     
-    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:UYLCountryCellIdentifier];
-
-    // *******************************************************************************
-    // If you are targetting iOS 5 there is a serious bug here with VoiceOver active!!
-    // *******************************************************************************
-
-    // A bug in iOS 5 causes the above call to not return with a new cell when VoiceOver is active.
-    // We therefore fallback to the old mechanism of manually loading the cell from the NIB file.
-
-    // Unfortunately this has two side effects. Firstly we need to manually perform the segue
-    // when the row is selected. To ensure we only do that for the cells loaded manually we
-    // need to use a different reuseIdentifer.
-    
-    // More seriously since the reuse mechanism does not work when VoiceOver is active
-    // new cells are allocated each time the table is reloaded or scrolled and never reused.
-
-    if (cell == nil)
-    {
-        // The following call should return cells for reuse but when VoiceOver is active
-        // it always returns nil
-        
-        cell = [self.tableView dequeueReusableCellWithIdentifier:UYLNibCountryCellIdentifier];
-        
-        if (cell == nil)
-        {
-            [self.countryCellNib instantiateWithOwner:self options:nil];
-            cell = self.countryCell;
-            self.countryCell = nil;
-        }
-    }
-
     Country *country = nil;
-    if (tableView == self.tableView)
-    {
-        country = [self.fetchedResultsController objectAtIndexPath:indexPath];
-    }
-    else
+    if (self.searchController.active)
     {
         country = [self.filteredList objectAtIndex:indexPath.row];
     }
+    else
+    {
+        country = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    }
     
-    UILabel *nameLabel = (UILabel *)[cell viewWithTag:UYL_COUNTRYCELLTAG_NAME];
-    nameLabel.text = country.name;
-
-    UILabel *capLabel = (UILabel *)[cell viewWithTag:UYL_COUNTRYCELLTAG_CAPITAL];
-    capLabel.text = country.capital;
-
-    UILabel *popLabel = (UILabel *)[cell viewWithTag:UYL_COUNTRYCELLTAG_POP];
+    cell.countryLabel.text = country.name;
+    cell.capitalLabel.text = country.capital;
+    
     NSString *population = [self.decimalFormatter stringFromNumber:country.population];
-    popLabel.text = [NSString stringWithFormat:NSLocalizedString(@"Pop: %@", @"Pop:"), population];   
+    cell.populationLabel.text = [NSString stringWithFormat:NSLocalizedString(@"Pop: %@", @"Pop:"), population];
+    
+    cell.countryLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+    cell.capitalLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline];
+    cell.populationLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline];
     
     return cell;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
-    if (tableView == self.tableView)
+    if (!self.searchController.active)
     {
         id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:section];
         return [sectionInfo name];
     }
-    else
-    {
-        return nil;
-    }
+    return nil;
 }
 
 - (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView
 {
-    if (tableView == self.tableView)
+    if (!self.searchController.active)
     {
         NSMutableArray *index = [NSMutableArray arrayWithObject:UITableViewIndexSearch];
         NSArray *initials = [self.fetchedResultsController sectionIndexTitles];
         [index addObjectsFromArray:initials];
         return index;
     }
-    else
-    {
-        return nil;
-    }
+    return nil;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index
 {
-    if (tableView == self.tableView)
+    if (!self.searchController.active)
     {
         if (index > 0)
         {
@@ -292,31 +253,12 @@ static NSString *UYLSegueShowCountry = @"UYLSegueShowCountry";
             // The first entry in the index is for the search icon so we return section not found
             // and force the table to scroll to the top.
             
-            self.tableView.contentOffset = CGPointZero;
+            CGRect searchBarFrame = self.searchController.searchBar.frame;
+            [self.tableView scrollRectToVisible:searchBarFrame animated:NO];
             return NSNotFound;
         }
     }
-    else
-    {
-        return 0;
-    }
-}
-
-#pragma mark -
-#pragma mark === UITableViewDelegate ===
-#pragma mark -
-
-// The following method is implemented as a workaround for the iOS 5 bug when dequeuing
-// cells with VoiceOver active. When the cell is not loaded from the storyboard
-// we need to manually perform the segue.
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-    if ([cell.reuseIdentifier isEqualToString:UYLNibCountryCellIdentifier])
-    {
-        [self performSegueWithIdentifier:UYLSegueShowCountry sender:cell];
-    }
+    return 0;
 }
 
 #pragma mark -
@@ -330,26 +272,29 @@ static NSString *UYLSegueShowCountry = @"UYLSegueShowCountry";
         return _fetchedResultsController;
     }
     
-    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
-    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Country" inManagedObjectContext:self.managedObjectContext];
-    [fetchRequest setEntity:entity];
-    
-    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];
-    NSArray *sortDescriptors = [NSArray arrayWithObjects:sortDescriptor, nil];
-    [fetchRequest setSortDescriptors:sortDescriptors];
-    
-    NSFetchedResultsController *frc = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
-                                                                          managedObjectContext:self.managedObjectContext 
-                                                                            sectionNameKeyPath:@"sectionTitle"
-                                                                                     cacheName:@"Country"];
-    frc.delegate = self;
-    self.fetchedResultsController = frc;
-    
-	NSError *error = nil;
-	if (![self.fetchedResultsController performFetch:&error])
+    if (self.managedObjectContext)
     {
-	    NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-	}
+        NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+        NSEntityDescription *entity = [NSEntityDescription entityForName:@"Country" inManagedObjectContext:self.managedObjectContext];
+        [fetchRequest setEntity:entity];
+        
+        NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];
+        NSArray *sortDescriptors = [NSArray arrayWithObjects:sortDescriptor, nil];
+        [fetchRequest setSortDescriptors:sortDescriptors];
+        
+        NSFetchedResultsController *frc = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
+                                                                              managedObjectContext:self.managedObjectContext
+                                                                                sectionNameKeyPath:@"sectionTitle"
+                                                                                         cacheName:@"Country"];
+        frc.delegate = self;
+        self.fetchedResultsController = frc;
+        
+        NSError *error = nil;
+        if (![self.fetchedResultsController performFetch:&error])
+        {
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        }
+    }
     
     return _fetchedResultsController;
 }    
@@ -360,26 +305,23 @@ static NSString *UYLSegueShowCountry = @"UYLSegueShowCountry";
 }
 
 #pragma mark -
-#pragma mark === UISearchDisplayDelegate ===
+#pragma mark === UISearchBarDelegate ===
 #pragma mark -
 
-- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+- (void)searchBar:(UISearchBar *)searchBar selectedScopeButtonIndexDidChange:(NSInteger)selectedScope
 {
-    UYLWorldFactsSearchScope scopeKey = controller.searchBar.selectedScopeButtonIndex;
-    [self searchForText:searchString scope:scopeKey];
-    return YES;
+    [self updateSearchResultsForSearchController:self.searchController];
 }
 
-- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption
-{
-    NSString *searchString = controller.searchBar.text;
-    [self searchForText:searchString scope:searchOption];
-    return YES;
-}
+#pragma mark -
+#pragma mark === UISearchResultsUpdating ===
+#pragma mark -
 
-- (void)searchDisplayController:(UISearchDisplayController *)controller didLoadSearchResultsTableView:(UITableView *)tableView
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController
 {
-    tableView.rowHeight = 64;
+    NSString *searchString = searchController.searchBar.text;
+    [self searchForText:searchString scope:searchController.searchBar.selectedScopeButtonIndex];
+    [self.tableView reloadData];
 }
 
 - (void)searchForText:(NSString *)searchText scope:(UYLWorldFactsSearchScope)scopeOption
