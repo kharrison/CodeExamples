810f0fd44a17bca7c330d3bef80b7b0a5444e474
Keith Harrison <krh007@gmail.com>
2015-05-18 22:27:07 +0100

    Add AnimatedConstraints

---
+++ AnimatedConstraints/AnimatedConstraints/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnimatedConstraints
+//
+//  Created by Keith Harrison on 17/05/2015.
+//  Copyright (c) 2015 Keith Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
