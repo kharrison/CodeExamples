0eea3dc7e1f2bf298d9c167319b37a403fea6509
Keith Harrison <krh007@gmail.com>
2016-04-25 19:04:30 +0100

    Add StaticTableDynamicType

---
+++ StaticTableDynamicType/README.md
@@ -0,0 +1,9 @@
+### Static Table View Support for Dynamic Type
+
+A look at how to get dynamic type working with static table views. See the following post further details:
+
++ [Static Tables and Dynamic Type](http://useyourloaf.com/blog/static-tables-and-dynamic-type/)
+
+#### Version History
+
++ Version 1.0  25 Apr 2016    Initial Version
---
+++ StaticTableDynamicType/StaticTable/UYLBasicStaticTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  UYLBasicStaticTableViewCell.swift
+//  StaticTable
+//
+//  Created by Keith Harrison on 21/04/2016.
+//  Copyright © 2016 Keith Harrison. All rights reserved.
+//
+
+import UIKit
+
+class UYLBasicStaticTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleText: UILabel!
+}
+
+extension UYLBasicStaticTableViewCell: UYLPreferredFont {
+    func contentSizeChanged() {
+        titleText.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+    }
+}
