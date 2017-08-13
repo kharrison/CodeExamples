c18243008b6b6a247fc8d6f940bb78af70754796
Keith Harrison <keith@useyourloaf.com>
2017-08-13 22:24:17 +0100

    Add ScaledFont

---
+++ ScaledFont/README.md
@@ -0,0 +1,44 @@
+#  Scaled Font
+
+## Using Dynamic Type With Custom Fonts
+
+An example of using custom fonts with dynamic type making use of the `UIFontMetrics` class introduced with iOS 11. It provides examples using the Noteworthy font which is built-in to iOS and the NotoSerif font which was downloaded from google fonts:
+
++ [Noto Sans]([NotoSans](https://fonts.google.com/specimen/Noto+Serif?selection.family=Noto+Serif))
+
+*Refer to LICENSE.txt if you plan on using NotoSans in a shipping application.*
+
+**This project requires iOS 11**
+
+### ScaledFont
+
+A utility class to help you use custom fonts with dynamic type.
+
+To use this class you must supply the name of a style dictionary for the font when creating the class. The style dictionary should be stored as a property list file in the main bundle.
+
+The style dictionary contains an entry for each text style that uses the raw string value for each `UIFontTextStyle` as the key.
+
+The value of each entry is a dictionary with two keys:
+
++ fontName: A String which is the font name.
++ fontSize: A number which is the point size to use at the `.large` content size.
+
+For example to use a 17 pt Noteworthy-Bold font for the `.headline` style at the `.large` content size:
+
+    <dict>
+        <key>UICTFontTextStyleHeadline</key>
+        <dict>
+            <key>fontName</key>
+            <string>Noteworthy-Bold</string>
+            <key>fontSize</key>
+            <integer>17</integer>
+        </dict>
+    </dict>
+
+You do not need to include an entry for every text style but if you try to use a text style that is not included in the dictionary it will fallback to the system preferred font.
+
+### Further Details
+
+See the following blog post for further details:
+
++ [Using A Custom Font With Dynamic Type](https://useyourloaf.com/blog/using-a-custom-font-with-dynamic-type/)
---
+++ ScaledFont/ScaledFont/ViewController.swift
@@ -0,0 +1,125 @@
+//  Created by Keith Harrison https://useyourloaf.com
+//  Copyright (c) 2017 Keith Harrison. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//  notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright
+//  notice, this list of conditions and the following disclaimer in the
+//  documentation and/or other materials provided with the distribution.
+//
+//  3. Neither the name of the copyright holder nor the names of its
+//  contributors may be used to endorse or promote products derived from
+//  this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//    private let fontName = "Default"
+//    private let fontName = "Noteworthy"
+    private let fontName = "NotoSerif"
+
+    private let defaultSpacing: CGFloat = 8.0
+
+    private lazy var scaledFont: ScaledFont = {
+        return ScaledFont(fontName: fontName)
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = fontName
+        setupViews()
+    }
+
+    private lazy var title1Label: UILabel = {
+        return label(forTextStyle: .title1, text: "Title 1")
+    }()
+
+    private lazy var title2Label: UILabel = {
+        return label(forTextStyle: .title2, text: "Title 2")
+    }()
+
+    private lazy var title3Label: UILabel = {
+        return label(forTextStyle: .title3, text: "Title 3")
+    }()
+
+    private lazy var headlineLabel: UILabel = {
+        return label(forTextStyle: .headline, text: "Headline")
+    }()
+
+    private lazy var subheadlineLabel: UILabel = {
+        return label(forTextStyle: .subheadline, text: "Subheadline")
+    }()
+
+    private lazy var bodyLabel: UILabel = {
+        return label(forTextStyle: .body, text: "Body")
+    }()
+
+    private lazy var calloutLabel: UILabel = {
+        return label(forTextStyle: .callout, text: "Callout")
+    }()
+
+    private lazy var footnoteLabel: UILabel = {
+        return label(forTextStyle: .footnote, text: "Footnote")
+    }()
+
+    private lazy var caption1Label: UILabel = {
+        return label(forTextStyle: .caption1, text: "Caption 1")
+    }()
+
+    private lazy var caption2Label: UILabel = {
+        return label(forTextStyle: .caption2, text: "Caption 2")
+    }()
+
+    private func label(forTextStyle textStyle: UIFontTextStyle, text: String) -> UILabel {
+        let label = UILabel()
+        label.font = scaledFont.font(forTextStyle: textStyle)
+        label.adjustsFontForContentSizeCategory = true
+        label.text = text
+        label.numberOfLines = 0
+        return label
+    }
+
+    private func setupViews() {
+        let stackView = UIStackView(arrangedSubviews: [title1Label, title2Label, title3Label, headlineLabel, subheadlineLabel, bodyLabel, calloutLabel, footnoteLabel, caption1Label, caption2Label])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = defaultSpacing
+        stackView.axis = .vertical
+
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(stackView)
+        view.addSubview(scrollView)
+
+        let margins = view.readableContentGuide
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: margins.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: margins.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+
+            scrollView.topAnchor.constraint(equalTo: stackView.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+
+            scrollView.widthAnchor.constraint(equalTo: stackView.widthAnchor)
+            ])
+    }
+}
