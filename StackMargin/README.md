f2bd003441293d768cacd1298b861acc1e8a803d
Keith Harrison <keith@useyourloaf.com>
2018-09-10 17:08:52 +0100

    Add StackMargin project

---
+++ StackMargin/README.md
@@ -0,0 +1,7 @@
+#  Adding Padding To A StackView
+
+How to use layout margins and the readable content guide to add padding to a stack view. The sample project shows how to do this with Interface Builder and when working with programmatic layouts.
+
+For further details see the blog post:
+
++ [Adding padding to a stack view](https://useyourloaf.com/blog/adding-padding-to-a-stack-view/)
---
+++ StackMargin/StackMargin/CodeViewController.swift
@@ -0,0 +1,114 @@
+//  Copyright © 2018 Keith Harrison. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//  notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright
+//  notice, this list of conditions and the following disclaimer in the
+//  documentation and/or other materials provided with the distribution.
+//
+//  3. Neither the name of the copyright holder nor the names of its
+//  contributors may be used to endorse or promote products derived from
+//  this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+
+import UIKit
+
+final class CodeViewController: UIViewController {
+
+    private let chapterLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.numberOfLines = 0
+        label.adjustsFontForContentSizeCategory = true
+        label.backgroundColor = .white
+        return label
+    }()
+
+    private let textView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.adjustsFontForContentSizeCategory = true
+        textView.isScrollEnabled = false
+        textView.isEditable = false
+        return textView
+    }()
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [chapterLabel, textView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = UIStackView.spacingUseSystem
+        return stackView
+    }()
+
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+        view.addSubview(stackView)
+        view.backgroundColor = .red
+
+        let readable = view.readableContentGuide
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: readable.leadingAnchor),
+            stackView.topAnchor.constraint(equalTo: readable.topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: readable.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: readable.bottomAnchor),
+            ])
+        return view
+    }()
+
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(containerView)
+        return scrollView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+
+        chapterLabel.text = "Down the Rabbit-Hole"
+        textView.text = """
+        Alice was beginning to get very tired of sitting by her sister on the \
+        bank, and of having nothing to do: once or twice she had peeped into the \
+        book her sister was reading, but it had no pictures or conversations in \
+        it, ‘and what is the use of a book,’ thought Alice ‘without pictures or \
+        conversations?’
+        """
+    }
+
+    private func setupView() {
+        view.addSubview(scrollView)
+
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            scrollView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            scrollView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+
+            scrollView.widthAnchor.constraint(equalTo: containerView.widthAnchor)
+            ])
+    }
+}
