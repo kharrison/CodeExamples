aa32ccf8b274717a74c5765ee2de3bfa82137c6c
Ralf Ebert <info@ralfebert.de>
2019-03-16 16:30:42 +0100

    AdaptivePopover updated to Xcode 10 + Swift 5

--- AdaptivePopover/AdaptivePopover/RootViewController.swift
+++ AdaptivePopover/AdaptivePopover/RootViewController.swift
@@ -38,20 +38,20 @@ class RootViewController: UIViewController {
     @IBOutlet weak var simpleButton: UIButton!
     @IBOutlet weak var embeddedButton: UIButton!
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         switch segue.identifier {
         case "SimpleSegue"?:
-            let simplePPC = segue.destinationViewController.popoverPresentationController
+            let simplePPC = segue.destination.popoverPresentationController
             simplePPC?.delegate = self
             simplePPC?.sourceView = simpleButton
             simplePPC?.sourceRect = simpleButton.bounds
         case "EmbeddedSegue"?:
-            let embeddedPPC = segue.destinationViewController.popoverPresentationController
+            let embeddedPPC = segue.destination.popoverPresentationController
             embeddedPPC?.delegate = self
             embeddedPPC?.sourceView = embeddedButton
             embeddedPPC?.sourceRect = embeddedButton.bounds
         default:
-            fatalError("Unknown segue: \(segue.identifier)")
+            fatalError("Unknown segue: \(String(describing: segue.identifier))")
         }
     }
 }
@@ -64,17 +64,17 @@ extension RootViewController: UIPopoverPresentationControllerDelegate {
     // to allow it to be dismissed. Handle the situation where
     // our popover may be embedded in a navigation controller
     
-    func presentationController(controller: UIPresentationController, viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle) -> UIViewController? {
-        guard style != .None else {
+    func presentationController(_ controller: UIPresentationController, viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle) -> UIViewController? {
+        guard style != .none else {
             return controller.presentedViewController
         }
         
         if let navController = controller.presentedViewController as? UINavigationController {
-            addDismissButton(navController)
+            addDismissButton(navigationController: navController)
             return navController
         } else {
             let navController = UINavigationController.init(rootViewController: controller.presentedViewController)
-            addDismissButton(navController)
+            addDismissButton(navigationController: navController)
             return navController
         }
     }
@@ -82,22 +82,22 @@ extension RootViewController: UIPopoverPresentationControllerDelegate {
     // Check for when we present in a non modal style and remove the
     // the dismiss button from the navigation bar.
     
-    func presentationController(presentationController: UIPresentationController, willPresentWithAdaptiveStyle style: UIModalPresentationStyle, transitionCoordinator: UIViewControllerTransitionCoordinator?) {
-        if style == .None {
+    func presentationController(_ presentationController: UIPresentationController, willPresentWithAdaptiveStyle style: UIModalPresentationStyle, transitionCoordinator: UIViewControllerTransitionCoordinator?) {
+        if style == .none {
             if let navController = presentationController.presentedViewController as? UINavigationController {
-                removeDismissButton(navController)
+                removeDismissButton(navigationController: navController)
             }
         }
     }
     
-    func didDismissPresentedView() {
-        presentedViewController?.dismissViewControllerAnimated(true, completion: nil)
+    @objc func didDismissPresentedView() {
+        presentedViewController?.dismiss(animated: true, completion: nil)
     }
     
     private func addDismissButton(navigationController: UINavigationController) {
         let rootViewController = navigationController.viewControllers[0]
-        rootViewController.navigationItem.leftBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .Done,
-            target: self, action: "didDismissPresentedView")
+        rootViewController.navigationItem.leftBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .done,
+                                                                                   target: self, action: #selector(didDismissPresentedView))
     }
     
     private func removeDismissButton(navigationController: UINavigationController) {
